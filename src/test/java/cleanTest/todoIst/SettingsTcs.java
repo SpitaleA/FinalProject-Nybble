53ec875bc4f7612c8781e6c6d5124cb1a9eb3381
SpitaleA <agute.jcr@gmail.com>
2022-11-07 22:28:28 -0300

    TC auth completos, TC settings: register(),registerAndDeleteAccount(),logOut, changelanguage and change username ready

--- src/test/java/cleanTest/todoIst/SettingsTcs.java
+++ src/test/java/cleanTest/todoIst/SettingsTcs.java
@@ -11,58 +11,6 @@ import utils.RandomString;
 import static utils.RandomString.getAlphaNumericString;
 
 public class SettingsTcs extends TestBaseTodoIst{
-    @Test
-    public void register() throws InterruptedException {
-        String email = getAlphaNumericString(6) + "@gmail.com";
-        String pwd = getAlphaNumericString(8);
-
-        mainPage.registerButton.click();
-        registerPage.emailTextbox.setText(email);
-        registerPage.pwdTextbox.setText(pwd);
-        registerPage.registerBtn.click();
-        loggedinMainPage_CenterTasksArea.skipStart.click();
-        Assertions.assertTrue(loggedinMainPage_Navbar.addTask.isControlDisplayed(),"Error user was not registred correctly");
-        Thread.sleep(10000);
-
-
-    }
-    @Test
-    public void registerAndDeleteAccount() throws InterruptedException {
-        String email = getAlphaNumericString(6) + "@gmail.com";
-        String pwd = getAlphaNumericString(8);
-
-        mainPage.registerButton.click();
-        registerPage.emailTextbox.setText(email);
-        registerPage.pwdTextbox.setText(pwd);
-        registerPage.registerBtn.click();
-        loggedinMainPage_CenterTasksArea.skipStart.click();
-        Assertions.assertTrue(loggedinMainPage_Navbar.addTask.isControlDisplayed(),"Error user was not registred correctly");
-//        Thread.sleep(10000);
-        loggedinMainPage_Navbar.accountBtn.click();
-        loggedinMainPage_Navbar.configurationBtn.click();
-        settingsModal.deleteAccountBtn.click();
-        settingsModal.pwdDeleteTextbox.setText(pwd);
-        settingsModal.confirmDeleteBtn.click();
-        Assertions.assertEquals("https://todoist.com/auth/account-deleted",
-                Session.getInstance().getBrowser().getCurrentUrl(),
-                "Error account was not deleted");
-    }
-
-    @Test
-    public void logoutTest() throws InterruptedException {
-
-        //LOGIN
-        mainPage.loginButton.click();
-        loginPage.login(email ,pwd);
-
-        Assertions.assertTrue(loggedinMainPage_Navbar.addTask.isControlDisplayed(),"Error user was not logged correctly");
-
-        //LOGOUT
-        loggedinMainPage_Navbar.accountBtn.click();
-        loggedinMainPage_Navbar.logoutBtn.click();
-
-        Assertions.assertTrue(mainPage.loginButton.isControlDisplayed(),"ERROR user was not logged out");
-    }
 
     @Test
     public void changePageLanguage() throws InterruptedException {
